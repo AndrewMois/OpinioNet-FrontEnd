51efe1f52d6f47940d3b113c7d924318a8709441
Andrew <amoiseyenko@ukr.net>
2023-07-14 13:23:06 -0400

    test with removing outline for nav buttons

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -6,8 +6,8 @@ import {motion} from "framer-motion";
 
 function NavButton({href, imagePath, alt, text}) {
     return (
-        <motion.div whileTap={{scale: 0.8}} className="flex flex-col items-center justify-center">
-            <Link className="flex flex-col items-center justify-center"
+        <motion.div whileTap={{scale: 0.8}} className="flex flex-col items-center justify-center focus:outline-none">
+            <Link className="flex flex-col items-center justify-center focus:outline-none"
                   href={`${href}`}>
                 <Image src={`${imagePath}`} alt={alt} height="24" width="30" className="mb-1"/>
                 <span className="text-xs">{text}</span>
