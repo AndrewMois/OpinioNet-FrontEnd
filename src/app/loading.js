5c8b4efaf83e8805d0159426805b94344a7096f8
Andrew <amoiseyenko@ukr.net>
2023-07-14 21:10:52 -0400

    skeleton loaders added

---
+++ src/app/loading.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Loading() {
+    return (
+        <div className="px-4 mb-20">
+            <h1 className="text-2xl font-bold my-4">Feed</h1>
+            <div className="rounded-md my-4 animate-pulse">
+                {Array(5).fill(0).map((index) => (
+                    <div
+                        key={index}
+                        className="bg-white rounded-2xl box-shadow-black p-4 mb-4 border-2 border-black">
+                        <div className="flex justify-between items-center">
+                            <div className="w-3/4 h-6 bg-gray-300 rounded"></div>
+                            <div className="flex items-center justify-between gap-2">
+                                <div className="w-6 h-6 bg-gray-300 rounded"></div>
+                                <div className="w-8 h-6 bg-gray-300 rounded"></div>
+                            </div>
+                        </div>
+                        <div className="h-4 bg-gray-300 rounded mt-2"></div>
+                        <div className="h-4 bg-gray-300 rounded mt-2"></div>
+                        <div className="h-4 bg-gray-300 rounded mt-2"></div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Loading;
