a340294ee8b0636222ee700e0fbe23ddd640a541
Andrew <amoiseyenko@ukr.net>
2023-07-27 11:43:25 -0400

    animation for buttons added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,6 +7,7 @@ import ErrorMessage from "./ErrorMessage";
 import {useRouter} from 'next/navigation';
 import {useAuthContext} from "./Authentication";
 import {GridLoader} from "react-spinners";
+import {motion} from "framer-motion";
 
 function LoginForm() {
     const {register, handleSubmit, formState: {errors}} = useForm();
@@ -87,16 +88,25 @@ function LoginForm() {
                     <FieldValidationError message="Password is required"/>)}
             </div>
 
-            <div className="flex justify-around">
-                <button type="submit"
-                        className="bg-fuchsia-800 font-bold hover:bg-fuchsia-600 text-white px-4 py-2 rounded box-shadow-black">
-                    Login
-                </button>
-                <Link href="/register"
-                      className="text-fuchsia-800 font-bold hover:text-fuchsia-600 px-4 py-2 rounded border-2 border-black box-shadow-black">
-                    Register
-                </Link>
+            <div className="flex justify-around items-center">
+                <motion.div whileTap={{scale: 0.9}}>
+                    <button
+                        type="submit"
+                        className="bg-fuchsia-800 font-bold hover:bg-fuchsia-700 text-white px-4 py-2 border-2 border-black rounded box-shadow-black flex items-center"
+                    >
+                        Login
+                    </button>
+                </motion.div>
+                <motion.div whileTap={{scale: 0.9}}>
+                    <Link
+                        href="/register"
+                        className="text-fuchsia-800 font-bold hover:text-fuchsia-600 px-4 py-2 rounded border-2 border-black box-shadow-black flex items-center"
+                    >
+                        Register
+                    </Link>
+                </motion.div>
             </div>
+
             {loading && (
                 <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
                     <GridLoader color="#86198f" size={20}/>
