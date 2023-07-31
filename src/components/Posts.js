d4b67d1c93c98fde679bfec93e6f7346273cb3d1
peruso <michaelyuta@live.jp>
2023-07-30 20:16:10 -0400

    Created handleLike Func to add a like

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,16 +3,48 @@ import Image from "next/image";
 import InfiniteLoading from "./InfiniteLoading";
 
 
-const Posts = ({posts}) => {
+const Posts = ({ posts }) => {
 
-    function handleLike(postId) {
-        // Placeholder function for liking a post (to be replaced with a database update)
-        console.log(`Post ${postId} liked!`);
+    async function handleLike(postId) {
+
+        const user_id = sessionStorage.getItem('user_id');
+        const token = sessionStorage.getItem('token');
+        if (user_id && token) {
+
+            try {
+                // Placeholder function for liking a post (to be replaced with a database update)
+                console.log(`Post ${postId} liked!`);
+
+                const res = await fetch(`https://opinio-net-api-794h.vercel.app/api/api/microposts/${postId}/likes`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Accept": "application/json",
+                        },
+                        body: JSON.stringify({
+
+                            "user_id": user_id,
+                        }),
+                    });
+                if (!res.ok) {
+                    console.error('Error liking post:', res.statusText);
+                    console.log(user_id);
+
+                    return [];
+                }
+                console.log(`Post ${postId} liked!`);
+
+            } catch (error) {
+                console.error('Error liking post:', error);
+            }
+        }
+
     }
 
     if (posts.length === 0) {
         return (
-            <InfiniteLoading/>
+            <InfiniteLoading />
         )
     }
 
@@ -29,8 +61,8 @@ const Posts = ({posts}) => {
                             <span>{post.likes}</span>
                             <button>
                                 <Image src="/images/like.svg" alt="like" width={19} height={19}
-                                       className="bg-fuchsia-800 h-min p-1 box-content rounded-lg"
-                                       onClick={() => handleLike(post.id)}/>
+                                    className="bg-fuchsia-800 h-min p-1 box-content rounded-lg"
+                                    onClick={() => handleLike(post.id)} />
                             </button>
                         </div>
                     </div>
