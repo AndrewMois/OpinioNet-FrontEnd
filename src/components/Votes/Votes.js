68542e755d9741d2a420037bc066bbe6d2f8663c
peruso <michaelyuta@live.jp>
2023-08-05 00:17:08 -0400

    Adjusted design for vote graph

--- src/components/Votes/Votes.js
+++ src/components/Votes/Votes.js
@@ -4,16 +4,18 @@ import ButtonComponent from './ButtonComponent';
 
 const Votes = ({ agree_count, not_sure_count, disagree_count, user_id, token }) => {
 
+  const totalVotes = agree_count + not_sure_count + disagree_count;
+
   const votes = [
-    { id: 1, subject: 'Agree', count: agree_count },
-    { id: 2, subject: 'Not Sure', count: not_sure_count },
-    { id: 3, subject: 'Disagree', count: disagree_count },
+    { id: 1, subject: 'Agree', count: agree_count, percentage: (agree_count / totalVotes) * 100 },
+    { id: 2, subject: 'Not Sure', count: not_sure_count, percentage: (not_sure_count / totalVotes) * 100 },
+    { id: 3, subject: 'Disagree', count: disagree_count, percentage: (disagree_count / totalVotes) * 100 },
   ];
 
 
   return (
     <div className="bg-white rounded-2xl shadow-md p-4 mb-4 border-2 border-black">
-      <h1>Voting Results</h1>
+      <h1 className="font-bold">Voting Results</h1>
       <VotingComponent votes={votes} />
     </div>
   );
