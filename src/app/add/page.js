e47fb8af36874b7107f7cd205c0920916a6da760
peruso <michaelyuta@live.jp>
2023-08-05 00:16:23 -0400

    Minor adjustment

--- src/app/add/page.js
+++ src/app/add/page.js
@@ -1,20 +1,20 @@
 'use client';
 
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import TextArea from "../../components/TextArea";
-import {useRouter} from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import PageWrapper from "../../components/PageWrapper";
-import {useForm} from "react-hook-form";
-import {GridLoader} from "react-spinners";
-import {useAuthContext} from "../../components/Authentication";
+import { useForm } from "react-hook-form";
+import { GridLoader } from "react-spinners";
+import { useAuthContext } from "../../components/Authentication";
 
 
 function Add() {
 
-    const {push} = useRouter();
+    const { push } = useRouter();
     const [posting, setPosting] = React.useState(false);
-    const {register, handleSubmit, formState: {errors}} = useForm();
-    const {checkToken} = useAuthContext();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { checkToken } = useAuthContext();
 
     // Check if the token exists in storage on page load
     useEffect(() => {
@@ -78,7 +78,7 @@ function Add() {
                                 className="w-full border border-fuchsia-800 px-4 py-2 rounded-2xl box-shadow-black"
 
                                 onChange={(e) => setTitle(e.target.value)}
-                                {...register("title", {required: true, maxLength: 30})}
+                                {...register("title", { required: true, maxLength: 30 })}
                             />
                             {errors.title && errors.title.type === "required" && (
                                 <p id="outlined_error_help" className="mt-2 ml-4 text-xs text-red-600">
@@ -93,7 +93,7 @@ function Add() {
                             <label htmlFor="content" className="block font-semibold mb-1">
                                 Question
                             </label>
-                            <TextArea registerFunc={register}/>
+                            <TextArea registerFunc={register} />
                             {errors.content && errors.content.type === "required" && (
                                 <p id="outlined_error_help" className="mt-2 ml-4 text-xs text-red-600">
                                     <span className="font-bold">Please, write something! </span></p>
@@ -110,7 +110,7 @@ function Add() {
                     </form>
                     {posting && (
                         <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
-                            <GridLoader color="#86198f" size={20}/>
+                            <GridLoader color="#86198f" size={20} />
                         </div>
                     )}
                 </div>
