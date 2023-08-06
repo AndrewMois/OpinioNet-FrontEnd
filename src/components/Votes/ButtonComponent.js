14a78b650466100d0f7a9fa58336f68f99facb1a
Andrew <amoiseyenko@ukr.net>
2023-08-06 16:18:11 -0400

    Button size adjusted

--- src/components/Votes/ButtonComponent.js
+++ src/components/Votes/ButtonComponent.js
@@ -6,7 +6,7 @@ const ButtonComponent = ({label, onClick}) => {
     return (
         <motion.div whileTap={{scale: 0.9}}>
             <button onClick={onClick}
-                    className="bg-fuchsia-800 hover:bg-fuchsia-700 text-white font-bold py-2 px-4 rounded box-shadow-black">
+                    className="bg-fuchsia-800 hover:bg-fuchsia-700 text-white text-sm font-bold py-2 px-4 rounded box-shadow-black">
                 {label}
             </button>
         </motion.div>
