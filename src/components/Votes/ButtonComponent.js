902708a65360559ecc17f60b24c263521b858c97
Andrew <amoiseyenko@ukr.net>
2023-08-06 14:24:29 -0400

    Colours adjusted and tap effect added

--- src/components/Votes/ButtonComponent.js
+++ src/components/Votes/ButtonComponent.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {motion} from 'framer-motion';
 
-const ButtonComponent = ({ label, onClick }) => {
+const ButtonComponent = ({label, onClick}) => {
 
-  return (
-    <button onClick={onClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      {label}
-    </button>
-  );
+    return (
+        <motion.div whileTap={{scale: 0.9}}>
+            <button onClick={onClick}
+                    className="bg-fuchsia-800 hover:bg-fuchsia-700 text-white font-bold py-2 px-4 rounded box-shadow-black">
+                {label}
+            </button>
+        </motion.div>
+    );
 }
 
 export default ButtonComponent;
