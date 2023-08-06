8d82fc230ea0a2ab17018835aa8e7ff7e810c251
Andrew <amoiseyenko@ukr.net>
2023-08-06 15:24:07 -0400

    Delete button click animation added

--- src/components/Votes/Votes.js
+++ src/components/Votes/Votes.js
@@ -2,23 +2,23 @@ import React from 'react';
 import VotingComponent from './VotingComponent';
 
 
-const Votes = ({ agree_count, not_sure_count, disagree_count, user_id, token }) => {
+const Votes = ({agree_count, not_sure_count, disagree_count, user_id, token}) => {
 
-  const totalVotes = agree_count + not_sure_count + disagree_count;
+    const totalVotes = agree_count + not_sure_count + disagree_count;
 
-  const votes = [
-    { id: 1, subject: 'Agree', count: agree_count, percentage: (agree_count / totalVotes) * 100 },
-    { id: 2, subject: 'Not Sure', count: not_sure_count, percentage: (not_sure_count / totalVotes) * 100 },
-    { id: 3, subject: 'Disagree', count: disagree_count, percentage: (disagree_count / totalVotes) * 100 },
-  ];
+    const votes = [
+        {id: 1, subject: 'Agree', count: agree_count, percentage: (agree_count / totalVotes) * 100},
+        {id: 2, subject: 'Not Sure', count: not_sure_count, percentage: (not_sure_count / totalVotes) * 100},
+        {id: 3, subject: 'Disagree', count: disagree_count, percentage: (disagree_count / totalVotes) * 100},
+    ];
 
 
-  return (
-    <div className="bg-white rounded-2xl shadow-md p-4 mb-4 border-2 border-black">
-      <h1 className="font-bold">Voting Results</h1>
-      <VotingComponent votes={votes} />
-    </div>
-  );
+    return (
+        <div className="p-4 border-t border-black">
+            {/*<h1 className="font-bold">Voting Results</h1>*/}
+            <VotingComponent votes={votes}/>
+        </div>
+    );
 
 
 };
